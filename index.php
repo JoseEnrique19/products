1aca37409c9aab29e7aa46cf4f90bb1190fd5e44
JoseEnrique19 <1715110333@utec-tgo.edu.mx>
2017-05-16 16:07:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-       $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-       $db_name = "j5j675phbav00q7d";
-       $db_user = "flk4xobuhzsegftv";
-       $db_password = "s7gehvjm9li44kc9";
+       $db_host = "gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+       $db_name = "h0vu9ni4el813k37";
+       $db_user = "kwcpfy9dd1nn1pxw";
+       $db_password = "h72sziiyzpjig20g";
        $connection = mysqli_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysqli_error());
         mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
           @mysqli_query("SET NAMES 'utf8'");
