c168dfb1b8e14bfc466db3a15732997d90699ff5
JoseEnrique19 <1715110333@utec-tgo.edu.mx>
2017-05-16 15:03:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
        $connection = mysqli_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysqli_error());
         mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
           @mysqli_query("SET NAMES 'utf8'");
-    $sql_query = "SELECT * FROM productos";
+    $sql_query = "SELECT * FROM tipos";
     $result = mysqli_query($connection, $sql_query);
     $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
